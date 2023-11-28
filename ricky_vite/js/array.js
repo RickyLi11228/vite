3f54989b0559b2709267e598a486f6c65e9c3e20
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2023-11-27 21:05:36 -0500

    super work

---
+++ ricky_vite/js/array.js
@@ -0,0 +1,43 @@
+fruits = [
+    {
+        name: "Banana",
+        img: 'https://static.vecteezy.com/system/resources/previews/015/100/096/original/bananas-transparent-background-free-png.png',
+        taste: "sweet",
+    },
+    {
+        name: "Chilli Pepper",
+        img: 'https://clipart-library.com/images/rTnGg9kEc.png',
+        taste: "spicy",
+    },
+    {
+        name: "Elder Berry",
+        img: 'https://static.vecteezy.com/system/resources/previews/025/229/898/non_2x/fresh-cluster-of-elderberry-isolated-on-transparent-background-free-png.png',
+        taste: "sweet",
+    },
+    {
+        name: "Fig",
+        img: 'https://www.pngmart.com/files/15/Fig-Fruit-Transparent-PNG.png',
+        taste: "sweet",
+    },
+    {
+        name: "Radish",
+        img: 'https://pngimg.com/d/radish_PNG10.png',
+        taste: "spicy",
+    },
+    {
+        name: "Orange",
+        img: 'https://pngimg.com/d/orange_PNG791.png',
+        taste: "sweet",
+    },
+    {
+        name: "Blueberry",
+        img: 'https://i.pinimg.com/originals/91/25/b2/9125b2471bb9514dd03f878f05ece4f4.png',
+        taste: "sweet",
+    },
+    {
+        name: "Mango",
+        img: 'https://static.vecteezy.com/system/resources/previews/015/100/096/original/bananas-transparent-background-free-png.png',
+        taste: "sour",
+    },
+];
+export {fruits};
---
+++ ricky_vite/js/dom.js
@@ -0,0 +1,13 @@
+import { fruits } from "./array";
+
+const DOMSelectors ={
+    card: document.querySelector(".card"),
+    img: document.querySelector(".img"),
+    name: document.querySelector(".name"),
+    sour: document.querySelector(".sour"),
+    sweet: document.querySelector(".sweet"),
+    spicy: document.querySelector(".spicy"),
+    btn: document.querySelector(".btn"),
+};
+
+export{DOMSelectors}
